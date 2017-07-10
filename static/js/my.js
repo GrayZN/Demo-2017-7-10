ce37304cfd164791890607eb8e9437ad28cafe18
GrayZN <GrayZN@users.noreply.github.com>
2017-07-10 13:20:03 +0800

    静态资源

---
+++ static/js/my.js
@@ -0,0 +1,17 @@
+function start() {
+    alert("Hello Word");
+}
+
+function cs() {
+    var dom = document.getElementById("cs");
+    dom.innerHTML = "1234567";
+    dom.style.border = "1px solid red";
+}
+
+var tableValue = {
+    name:"",
+    sex:"",
+    zy:"",
+    ah:"",
+    grjl:""
+}
